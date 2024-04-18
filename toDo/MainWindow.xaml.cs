abd1626973358df44cf920e3a218d91612fcf7fb
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-04-18 12:14:18 +0200

    kurva

--- toDo/MainWindow.xaml.cs
+++ toDo/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,6 +7,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
@@ -23,15 +25,32 @@ namespace toDo
 
         private void btn1_Click(object sender, RoutedEventArgs e)
         {
-            list.Items.Add((string)txtBox.Text);
+                  
+            list.Items.Add(txtBox.Text);
             txtBox.Clear();
-        }
-        private void btn2_Click(object sender, RoutedEventArgs e)
-        {
-            int szam = list.SelectedItems.IndexOf(list.SelectedItems);
-            list.SelectedIndex = szam;
             
         }
+        private void btn2_Click(object sender, RoutedEventArgs e)
+		{
+			list.Items.Remove(list.Items[list.SelectedIndex]);
+			list.Items.Add(txtBox.Text);
+
+
+			//         foreach (ListBoxItem item in list.SelectedItems)  
+			//         {
+			//             csere += item.Content.ToString();
+			//             if (txtCsere == csere)
+			//             {
+			//		MessageBox.Show("Ez ugyan az a szöveg.", "Save error", MessageBoxButton.OK, MessageBoxImage.Error);
+			//	}
+			//             else
+			//             {
+			//                 item.Content = txtCsere;
+			//             }
+			//}
+            txtBox.Clear();
+            
+		}
 
         private void btn3T_Click(object sender, RoutedEventArgs e)
         {
@@ -45,7 +64,8 @@ namespace toDo
             }
             
         }
-        private void btn4T_Click(object sender, RoutedEventArgs e)
+		/* kiválasztott elem törlése */
+		private void btn4T_Click(object sender, RoutedEventArgs e)
         {
             if (list.Items.Count > 0)
             {
@@ -56,10 +76,22 @@ namespace toDo
                 MessageBox.Show("A listbox üres, töltse fel mielött törölne.", "Save error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
-        private void kilep_Click(object sender, RoutedEventArgs e)
+        /* másolás */
+        private void btn5C_Click(object sender, RoutedEventArgs e)
+        {
+            string copyContent = string.Empty;
+            foreach (ListBoxItem item in list.SelectedItems)
+            {
+                copyContent += item.Content.ToString();
+            }
+
+            list_Copy.Items.Add(copyContent);
+        }
+
+		private void kilep_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
         }
 
-    }
+	}
 }
